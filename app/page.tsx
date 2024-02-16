a398af5dbc90201d7df1fae045e949c164dcec6f
Alexander Horn <alexhorn@users.noreply.github.com>
2024-02-16 22:34:24 +0100

    Redirect from root to step 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import styles from './page.module.css'
+import { redirect } from 'next/navigation'
 
 export default function Home () {
-  return (
-    <main className={styles.main}>
-      Hello
-    </main>
-  )
+  redirect('/step1')
 }
